22549a69edc8a4c1d11dd37b4a2c04f2d381d927
HUOYING-TA6X\cc <1945258983@qq.com>
2024-05-02 20:09:07 +0800

    添加window与 android相同方法

--- windows/keyboard_aids_plugin.cpp
+++ windows/keyboard_aids_plugin.cpp
@@ -13,47 +13,70 @@
 #include <memory>
 #include <sstream>
 
-namespace keyboard_aids {
+namespace keyboard_aids
+{
 
-// static
-void KeyboardAidsPlugin::RegisterWithRegistrar(
-    flutter::PluginRegistrarWindows *registrar) {
-  auto channel =
-      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "keyboard_aids",
-          &flutter::StandardMethodCodec::GetInstance());
+  // static
+  void KeyboardAidsPlugin::RegisterWithRegistrar(
+      flutter::PluginRegistrarWindows *registrar)
+  {
+    auto channel =
+        std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+            registrar->messenger(), "keyboard_aids",
+            &flutter::StandardMethodCodec::GetInstance());
 
-  auto plugin = std::make_unique<KeyboardAidsPlugin>();
+    auto plugin = std::make_unique<KeyboardAidsPlugin>();
 
-  channel->SetMethodCallHandler(
-      [plugin_pointer = plugin.get()](const auto &call, auto result) {
-        plugin_pointer->HandleMethodCall(call, std::move(result));
-      });
+    channel->SetMethodCallHandler(
+        [plugin_pointer = plugin.get()](const auto &call, auto result)
+        {
+          plugin_pointer->HandleMethodCall(call, std::move(result));
+        });
 
-  registrar->AddPlugin(std::move(plugin));
-}
-
-KeyboardAidsPlugin::KeyboardAidsPlugin() {}
-
-KeyboardAidsPlugin::~KeyboardAidsPlugin() {}
-
-void KeyboardAidsPlugin::HandleMethodCall(
-    const flutter::MethodCall<flutter::EncodableValue> &method_call,
-    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
-    }
-    result->Success(flutter::EncodableValue(version_stream.str()));
-  } else {
-    result->NotImplemented();
+    registrar->AddPlugin(std::move(plugin));
   }
-}
 
-}  // namespace keyboard_aids
+  KeyboardAidsPlugin::KeyboardAidsPlugin() {}
+
+  KeyboardAidsPlugin::~KeyboardAidsPlugin() {}
+
+  void KeyboardAidsPlugin::HandleMethodCall(
+      const flutter::MethodCall<flutter::EncodableValue> &method_call,
+      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
+  {
+
+    const std::string method_name = method_call.method_name();
+
+    if (method_name.compare("getPlatformVersion") == 0)
+    {
+      std::ostringstream version_stream;
+      version_stream << "Windows ";
+      if (IsWindows10OrGreater())
+      {
+        version_stream << "10+";
+      }
+      else if (IsWindows8OrGreater())
+      {
+        version_stream << "8";
+      }
+      else if (IsWindows7OrGreater())
+      {
+        version_stream << "7";
+      }
+      result->Success(flutter::EncodableValue(version_stream.str()));
+    }
+    else if (method_name.compare("open") == 0)
+    {
+      result->Success(flutter::EncodableValue("open"));
+    }
+    else if (method_name.compare("close") == 0)
+    {
+      result->Success(flutter::EncodableValue("close"));
+    }
+    else
+    {
+      result->NotImplemented();
+    }
+  }
+
+} // namespace keyboard_aids
